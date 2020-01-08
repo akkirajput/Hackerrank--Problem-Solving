960ca938f319932fda6f8a6ef828442ad50dedea
Ankit Singh Chauhan <ankitchouhan819@gmail.com>
2020-01-09 01:16:17 +0530

    Create Plus Minus.cpp

---
+++ Plus Minus.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long int n,count0=0,countpos=0,countneg=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>0)
+        {
+            countpos++;
+        }
+        else if(a[i]<0)
+        {
+            countneg++;
+        }
+        else{
+            count0++;
+        }
+    }
+
+    printf("%0.6f\n",(float)countpos/n);
+    printf("%0.6f\n",(float)countneg/n);
+    printf("%0.6f\n",(float)count0/n);
+    return 0;
+
+}
